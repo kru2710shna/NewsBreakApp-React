5dca1f98b07c4677110e43f07fbda5deea5bc8df
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-09-14 16:55:17 -0700

    Page: Rendering and next Page and Prev page

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,17 +1,18 @@
 import './App.css';
 
 import React, { Component } from 'react'
-import Navbar from './components/Navbar';
+
 import News from './components/News';
+import Navbar from './components/Navbar';
 
 export default class App extends Component {
+
   render() {
     return (
       <div>
-        <Navbar />    
-        <News />
+        <Navbar />
+        <News/>
       </div>
     )
   }
 }
-

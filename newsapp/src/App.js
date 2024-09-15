9ad0d07bcb6751ca3f130f0f869c752f4493899c
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2024-09-15 02:12:08 -0700

    Project Completed

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -10,19 +10,19 @@ import News from './components/News';
 import Navbar from './components/Navbar';
 
 export default class App extends Component {
-
+  pageSize = 15
   render() {
     return (
       <div>
         <Router>
           <Navbar />
           <Routes>
-            <Route exact path="/" element={<News key='general' pageSize={5} country='us' category='general' />} />
-            <Route exact path="/business" element={<News key='business' pageSize={5} country='us' category='business' />} />
-            <Route exact path="/sport" element={<News key='sports' pageSize={5} country='us' category='sports' />} />
-            <Route exact path="/health" element={<News key='health' pageSize={5} country='us' category='health' />} />
-            <Route exact path="/technology" element={<News key='technology' pageSize={5} country='us' category='technology' />} />
-            <Route exact path="/science" element={<News key='science' pageSize={5} country='us' category='science' />} />
+            <Route exact path="/" element={<News key='general' pageSize={this.pageSize} country='us' category='general' />} />
+            <Route exact path="/business" element={<News key='business' pageSize={this.pageSize} country='us' category='business' />} />
+            <Route exact path="/sport" element={<News key='sports' pageSize={this.pageSize} country='us' category='sports' />} />
+            <Route exact path="/health" element={<News key='health' pageSize={this.pageSize} country='us' category='health' />} />
+            <Route exact path="/technology" element={<News key='technology' pageSize={this.pageSize} country='us' category='technology' />} />
+            <Route exact path="/science" element={<News key='science' pageSize={this.pageSize} country='us' category='science' />} />
           </Routes>
         </Router>
       </div>
